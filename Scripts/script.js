f1620b9e2b2de4cccb05d11a57a27a25758105a4
Lord3dfx <mxl42@mail.ru>
2023-04-18 00:25:19 +0700

    add src index.ts

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById("btn")
 const xml = new XMLHttpRequest()
 xml.open("GET","https://lord3dfx.github.io/MirsulCardBuilder/cards")
 function btnclick(){
-    alert(xml.statusText)
+    alert(xml.status)
 }
 
 btn.addEventListener('click', btnclick)
