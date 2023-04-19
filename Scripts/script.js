e82dc03cd9cf3bc53ec2d4ff67ceafbc16f74d9d
Lord3dfx <mxl42@mail.ru>
2023-04-19 21:13:52 +0700

    testing...

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("btn")
 function btnclick(){
-    fetch('https://cardbuilder.onrender.com/')
+    fetch('https://cardbuilder.onrender.com/test')
         .then((response) => {
             return response.json();
         })
