e3d9e2a9472309c1851291188de5cca0882f8ef2
Lord3dfx <mxl42@mail.ru>
2023-04-19 22:53:59 +0700

    receiving arr of cards

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,20 @@
-const btn = document.getElementById("btn")
-let resp = await fetch('https://api.render.com/deploy/srv-cgun9bgdh87vhrlp1lgg?key=qjY601xbdT4', { mode: 'no-cors'})
-function btnclick(){
-    alert(resp.status)
-}
+const arr = document.querySelector("#arr")
 
-btn.addEventListener('click', btnclick)
+    fetch('https://cardbuilder.onrender.com/cards')
+        .then(response =>  response.json())
+        .then(data => {
+            for (item of data)
+            {
+                addCard(item.cardName, item.status)
+            }
+            console.log(data)
+        })
+
+function addCard(name, status){
+    const arrTitle = document.createElement("p")
+    const arrStatus = document.createElement("span")
+    const br = document.createElement("hr")
+    arr.append(arrTitle, arrStatus,br)
+    arrTitle.innerText = name
+    arrStatus.innerText = status
+}
