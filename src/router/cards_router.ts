1b7dd53b90ee2aaaf0fe944e341b0e825424e088
Lera <jgfnvb@mail.ru>
2024-04-10 20:08:28 +0700

    add get hello

--- src/router/cards_router.ts
+++ src/router/cards_router.ts
@@ -17,20 +17,23 @@ cardRouter.get('/:id',async (req, res) => {
     const cards =await cards_service.getOneCard(req.params.id)
     res.status(200).send(cards)
 })
-cardRouter.delete('/:id', (req, res) => {
+cardRouter.delete('/:id',async (req, res) => {
     const cards = cards_service.deleteOneCard(req.params.id)
     res.status(200).send(cards)
 })
-cardRouter.delete('/', (req, res) => {
+cardRouter.delete('/',async (req, res) => {
      cards_service.deleteAllCard()
     res.sendStatus(204)
 })
-cardRouter.put('/:id', (req, res) => {
+cardRouter.put('/:id',async (req, res) => {
     const card = cards_service.updateOneCard(req.params.id,req.body.cardName,req.body.status)
     if(card){
     res.status(204).send(card)}
         else{res.sendStatus(404)}})
-cardRouter.put('/', (req, res) => {
+cardRouter.put('/', async (req, res) => {
     const cards = cards_service.updateAllCards(req.body.cardName,req.body.status)
     res.status(204).send(cards)
+},
+cardRouter.get('hello'), async (req,res) =>{
+    res.status(200).send("Hello, pidors")
 })
